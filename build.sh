fc274d93f25576db2cf15571ff08f06c36eea1c0
basilstotz <basil.stotz@gmail.com>
2014-04-19 18:03:28 +0200

    *

--- build.sh
+++ build.sh
@@ -9,6 +9,9 @@ ltsp-build-client \
     --chroot $chroot \
     --mirror http://localhost:3142/ch.archive.ubuntu.com/ubuntu/ \
     --fat-client \
-    --fat-client-desktop ubuntu-desktop 
-    --config etc/ltsp/ltsp-build.conf
-
+    --fat-client-desktop ubuntu-desktop \
+    --config ./etc/ltsp/empty.conf
+    --debconf-seeds ./etc/ltsp/debconf.seeds \
+    --late-packages "language-pack-de language-pack-gnome-de \ 
+                     language-pack-kde-de hyphen-de libreoffice-I10n-de \
+                     myspell-de-ch mythes-de-ch emacs nmap xosview"
