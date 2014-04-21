35ec2c0f65fc169cce4014ce7ea999390b6f7442
basilstotz <basil.stotz@gmail.com>
2014-04-21 14:09:12 +0200

    typo

--- build.sh
+++ build.sh
@@ -3,20 +3,21 @@
 chroot="kiosk"
 
 ltsp-build-client \
+    --purge-chroot \
+    --chroot $chroot \
     --mirror http://localhost:3142/ch.archive.ubuntu.com/ubuntu/ \
     --updates-mirror http://ch.archive.ubuntu.com/ubuntu/ \
     --security-mirror http://localhost:3142/ch.archive.ubuntu.com/ubuntu/ \
     --arch i386 \
     --skipimage \
-    --purge-chroot \
-    --chroot $chroot \
     --install-debs-dir /etc/ltsp/debs \
     --debconf-seeds /etc/ltsp/debconf.seeds \
     --fat-client \
     --fat-client-desktop ubuntu-desktop \
+    --andrijn-update "http://172.16.42.10/ltsp-images" \
+    --ldm-themes-extra \
+    --copy-tftpboot \
     --language-support-german \
     --ldap-auth \
     --multiuser-wine \
-    --andrijn-update "http://172.16.42.10/ltsp-images" \
-    --ldm-themes-extra \
     --late-packages "e3 nmap xosview" | tee build.log
