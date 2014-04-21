b20f835220fb116e56eae8960040fd6ea10230c6
basilstotz <basil.stotz@gmail.com>
2014-04-21 10:39:54 +0200

    typos

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ ltsp-build-client \
     --purge-chroot \
     --chroot $chroot \
     --mirror http://localhost:3142/ch.archive.ubuntu.com/ubuntu/ \
-    --install-debs-dir /etc/ltsp/debs
+    --install-debs-dir /etc/ltsp/debs \
     --fat-client \
     --fat-client-desktop ubuntu-desktop \
-    --ldap-auth
-    --winery
-    --debconf-seeds /etc/ltsp/debconf.seeds
+    --ldap-auth \
+    --winery \
+    --debconf-seeds /etc/ltsp/debconf.seeds \
     --andrijn-update "http://172.16.42.10/ltsp-images" \
     --ldm-themes-extra \
     --late-packages "language-pack-de language-pack-gnome-de  
