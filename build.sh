cae62586c585897fcee0906b7586b062168645cf
basilstotz <basil.stotz@gmail.com>
2014-04-21 11:49:18 +0200

    changed names

--- build.sh
+++ build.sh
@@ -7,15 +7,14 @@ ltsp-build-client \
     --skipimage \
     --purge-chroot \
     --chroot $chroot \
-    --mirror http://localhost:3142/ch.archive.ubuntu.com/ubuntu/ \
     --install-debs-dir /etc/ltsp/debs \
     --fat-client \
     --fat-client-desktop ubuntu-desktop \
     --ldap-auth \
-    --winery \
+    --multiuser-wine \
     --debconf-seeds /etc/ltsp/debconf.seeds \
     --andrijn-update "http://172.16.42.10/ltsp-images" \
     --ldm-themes-extra \
     --late-packages "language-pack-de language-pack-gnome-de  
                      language-pack-kde-de hyphen-de libreoffice-l10n-de 
-                     myspell-de-ch mythes-de-ch emacs nmap xosview"
+                     myspell-de-ch mythes-de-ch emacs nmap xosview" | tee build.log
