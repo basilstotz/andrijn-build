042716276644bc274875176f480ed45145d361fa
basilstotz <basil.stotz@gmail.com>
2014-04-21 10:38:18 +0200

    added new plugins

--- build.sh
+++ build.sh
@@ -11,8 +11,11 @@ ltsp-build-client \
     --install-debs-dir /etc/ltsp/debs
     --fat-client \
     --fat-client-desktop ubuntu-desktop \
-    --andrijn-updater "http://172.16.42.10/ltsp-images" \
+    --ldap-auth
+    --winery
+    --debconf-seeds /etc/ltsp/debconf.seeds
+    --andrijn-update "http://172.16.42.10/ltsp-images" \
     --ldm-themes-extra \
     --late-packages "language-pack-de language-pack-gnome-de  
-                     language-pack-kde-de hyphen-de libreoffice-I10n-de 
+                     language-pack-kde-de hyphen-de libreoffice-l10n-de 
                      myspell-de-ch mythes-de-ch emacs nmap xosview"
