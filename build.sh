6652f4c5cf1b3015a625efa0c0fc594725679dd4
basilstotz <basil.stotz@gmail.com>
2014-04-23 13:36:15 +0200

    *

--- build.sh
+++ build.sh
@@ -2,27 +2,30 @@
 
 chroot="kiosk"
 
-ltsp-build-client \
+#ltsp-build-client \
+echo \
     --purge-chroot \
-    --chroot $chroot \
-    --mirror http://localhost:3142/ch.archive.ubuntu.com/ubuntu/ \
-    --updates-mirror http://ch.archive.ubuntu.com/ubuntu/ \
-    --security-mirror http://localhost:3142/ch.archive.ubuntu.com/ubuntu/ \
+    --chroot "$chroot" \
+    --mirror "http://localhost:3142/ch.archive.ubuntu.com/ubuntu/" \
+    --updates-mirror "http://localhost:3142/ch.archive.ubuntu.com/ubuntu/" \
+    --security-mirror "http://localhost:3142/ch.archive.ubuntu.com/ubuntu/" \
     --skip-upgrade \
     --arch i386 \
     --skipimage \
-    --install-debs-dir /etc/ltsp/debs \
-    --update-sshkeys \ 
-    --debconf-seeds /etc/ltsp/debconf.seeds \
+    --update-sshkeys \
+    --install-debs-dir "/etc/ltsp/debs" \
+    --debconf-seeds "/etc/ltsp/debconf.seeds" \
     --andrijn-update "http://172.16.42.10/ltsp-images" \
     --set-lts-conf \
     --ldap-auth \
     --copy-tftpboot \
+    --terminal-server \
     --fat-client \
-    --fat-client-desktop ubuntu-desktop \
+    --fat-client-desktop "ubuntu-desktop" \
     --ldm-themes-extra \
     --custom-launcher \
     --language-support-german \
     --multiuser-wine \
-    --package-lists "debug.list"
+    --package-lists "debug.list" | tee build.log
+
 
