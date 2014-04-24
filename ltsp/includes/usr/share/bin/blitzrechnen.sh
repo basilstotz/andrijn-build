2c3aa125c042c6bf5bec421f7bc1d3114d1600e1
basilstotz <basil.stotz@gmail.com>
2014-04-24 15:03:21 +0200

    *

--- ltsp/includes/usr/share/bin/blitzrechnen.sh
+++ ltsp/includes/usr/share/bin/blitzrechnen.sh
@@ -1,12 +1,9 @@
 #!/bin/sh
-#winery mspaint install
-#ltsp-localapps winery mspaint blitzrechnen/blitzrechnen.exe
 
-if [ -z "$LTSP_CLIENT" ]; then
-  wine /home/lehrmittel/blitzrechnen/blitzrechnen.exe
+if [ -z "$LTSP_FATCLIENT" ]; then
+  wine $(/usr/share/bin/wine-path)/blitzrechnen/blitzrechnen.exe
 else
-#  ltsp-localapps wine /home/lehrmittel/blitzrechnen/blitzrechnen.exe
-  ltsp-localapps wine /home/lehrmittel/blitzrechnen/blitzrechnen.exe
+  ltsp-localapps wine $(/usr/share/bin/wine-path)/blitzrechnen/blitzrechnen.exe
 fi
 
 exit
