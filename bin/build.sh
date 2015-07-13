73e445f6f7f484d6d11be24e791acb5bfdd68b45
basilstotz <basil.stotz@gmail.com>
2015-07-13 17:19:11 +0200

    save

--- bin/build.sh
+++ bin/build.sh
@@ -1,34 +1,47 @@
 #!/bin/sh
 
-chroot="kiosk"
+IMG_BASE="/home/stotz.basil/ltsp"
+CNF_BASE="/home/stotz.basil/andrijn-build/"
+
 
 for mntpoint in dev/pts dev proc sys; do
-sudo umount -f /opt/ltsp/$chroot/$mntpoint 2>/dev/null \
+sudo umount -f ${IMG_BASE}/i386/$mntpoint 2>/dev/null \
     || true
 done
 
-
-
+if ! mount | grep -q "/etc/ltsp/plugins" ; then
+   if ! test -d /etc/ltsp/plugins;then mkdir -p /etc/ltsp/plugins;fi
+   mount --bind ${CFG_BASE}/plugins /etc/ltsp/plugins
+fi
 
 
 ltsp-build-client \
     --copy-package-cache \
+    --keep-packages \
     --mirror "http://mirror.switch.ch/ftp/ubuntu/" \
-    --base "/home/stotz.basil/ltsp" \
+    --base $IMG_BASE \
     --purge-chroot \
     --arch i386 \
     --skipimage \
     --update-sshkeys \
-    --install-debs-dir "/etc/ltsp/debs" \
-    --debconf-seeds "/etc/ltsp/preseeds/debconf.seeds" \
-    --andrijn-update "http://172.16.42.1/ltsp-images" \
+    --install-debs-dir "${CNF_BASE}/debs" \
+    --debconf-seeds "${CNF_BASE}/preseeds/debconf.seeds" \
+    --andrijn-update "http://www.amxa.ch/ltsp-images" \
     --ldap-auth \
-    --terminal-server \
     --fat-client \
     --fat-client-desktop "ubuntu-desktop" \
-    --language-support-german \
-    --multimedia \
-    --package-lists "debug.list edu.list standard.list"  | tee build.log
+    --language-support-german  \
+ | tee build.log
+
+
+if  mount | grep -q "/etc/ltsp/plugins" ; then
+      umount  /etc/ltsp/plugins
+fi
+
+
+#    --andrijn-terminal-server \
+#    --multimedia \
+#    --package-lists "debug.list edu.list standard.list"  | tee build.log
 
 
 #     --mirror "http://localhost:3142/ch.archive.ubuntu.com/ubuntu/" \
--- bin/publish.sh
+++ bin/publish.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-chroot="kiosk"
+chroot="i386"
 
 
-cd /opt/ltsp/images
+cd /home/stotz.basil/ltsp/images/
 
 if ! test -f $chroot.img;then 
   echo "already published"
@@ -17,8 +17,8 @@ if test -z "$VERSION";then VERSION=0;fi
 VPLUS=$(( $VERSION + 1))
 NAME=$(printf "%06i.img" $VPLUS )
 
-cp $chroot.img $NAME
-rm $chroot.img* 
+mv $chroot.img $NAME
+#rm $chroot.img* 
 zsyncmake -u $NAME $NAME
 
 rm cur-image-*
